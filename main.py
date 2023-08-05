1b33113a4bacb6c8e665fc1e6e5716a3c53e3896
kk <kaede112053@gmail.com>
2023-08-05 19:38:10 +0900

    customized

--- main.py
+++ main.py
@@ -1,38 +1,75 @@
 import streamlit as st
+import numpy as np
 from PIL import Image, ImageEnhance, ImageOps
+import torchvision.transforms as transforms
+import torch
+import requests
 
-def enhance_image(image, option, parameter=1.0):
-    if option == "高画質化":
-        return image.resize((image.width * 2, image.height * 2), Image.LANCZOS)
-    elif option == "コントラスト調整":
-        enhancer = ImageEnhance.Contrast(image)
-        return enhancer.enhance(parameter)
-    elif option == "色反転化":
-        return ImageOps.invert(image)
+# ページのタイトルやアイコンを設定
+st.set_page_config(page_title="Cool Image Editor", page_icon=":art:", layout="wide")
+
+# ESRGANのモデルをダウンロード
+model_url = "https://github.com/xinntao/ESRGAN/releases/download/v0.4.1/ESRGAN_x4.pth"
+model_path = "ESRGAN_x4.pth"
+if not st.session_state.model_downloaded:
+    st.session_state.model_downloaded = True
+    response = requests.get(model_url)
+    with open(model_path, "wb") as f:
+        f.write(response.content)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = torch.load(model_path, map_location=device).to(device)
+model.eval()
+
+def enhance_image_esrgan(image, scale_factor=4):
+    transform = transforms.Compose([
+        transforms.Resize((image.size[1] * scale_factor, image.size[0] * scale_factor)),
+        transforms.ToTensor()
+    ])
+    image_tensor = transform(image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        enhanced_tensor = model(image_tensor).clamp(0.0, 1.0)
+    enhanced_image = transforms.ToPILImage()(enhanced_tensor.cpu().squeeze())
+    return enhanced_image
+
+def adjust_contrast(image, factor=1.5):
+    enhancer = ImageEnhance.Contrast(image)
+    return enhancer.enhance(factor)
+
+def invert_colors(image):
+    return ImageOps.invert(image)
 
 def main():
-    st.title("画像処理アプリ")
-    st.write("アップロードされた画像に対して、処理を選択してください。")
+    st.title("Cool Image Editor")
 
-    uploaded_image = st.file_uploader("画像を選択してください", type=["jpg", "jpeg", "png"])
+    uploaded_image = st.file_uploader("画像をアップロードしてください", type=["jpg", "jpeg", "png"])
 
     if uploaded_image is not None:
         image = Image.open(uploaded_image)
-        st.subheader("アップロードされた画像")
-        st.image(image, caption="アップロードされた画像", use_column_width=True)
 
-        option = st.selectbox("処理を選択してください", ["選択してください", "高画質化", "コントラスト調整", "色反転化"])
+        st.subheader("変換前と変換後")
 
-        if option != "選択してください":
-            if option == "コントラスト調整":
-                contrast_level = st.slider("コントラストの強度", 0.0, 2.0, 1.0, 0.1)
-                processed_image = enhance_image(image, option, contrast_level)
-            else:
-                processed_image = enhance_image(image, option)
+        col1, col2 = st.beta_columns(2)
 
-            st.subheader("処理後の画像")
-            st.image(processed_image, caption="処理後の画像", use_column_width=True)
+        with col1:
+            st.image(image, caption="変換前", use_column_width=True)
+
+        with col2:
+            st.sidebar.subheader("エフェクトを選択")
+            selected_effects = st.sidebar.multiselect("エフェクトを選んでください", ["高画質化", "コントラスト調整", "色反転"])
+
+            if "高画質化" in selected_effects:
+                enhanced_image = enhance_image_esrgan(image)
+                st.image(enhanced_image, caption="高画質化", use_column_width=True)
+
+            if "コントラスト調整" in selected_effects:
+                contrast_factor = st.sidebar.slider("コントラストの強度を調整", 0.5, 5.0, 1.5)
+                contrast_adjusted_image = adjust_contrast(image, contrast_factor)
+                st.image(contrast_adjusted_image, caption="コントラスト調整", use_column_width=True)
+
+            if "色反転" in selected_effects:
+                inverted_image = invert_colors(image)
+                st.image(inverted_image, caption="色反転", use_column_width=True)
 
 if __name__ == "__main__":
     main()
-
