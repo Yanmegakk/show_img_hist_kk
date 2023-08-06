6be8f5715554528a059fe89f329d199cc4937351
kk <kaede112053@gmail.com>
2023-08-06 14:25:54 +0900

    main_txt_fixed

--- main.py
+++ main.py
@@ -1,83 +1,87 @@
 import streamlit as st
 import numpy as np
-from PIL import Image, ImageOps, ImageEnhance
+from PIL import Image, ImageEnhance, ImageOps, ImageDraw
 import matplotlib.pyplot as plt
 
+# 高画質化
 def enhance_image(image, scale_factor=2):
     width, height = image.size
     new_width = width * scale_factor
     new_height = height * scale_factor
     return image.resize((new_width, new_height), Image.LANCZOS)
 
-def adjust_contrast(image, factor):
+# コントラスト調整
+def adjust_contrast(image, factor=1.5):
     enhancer = ImageEnhance.Contrast(image)
     return enhancer.enhance(factor)
 
+# 色反転化
 def invert_colors(image):
     return ImageOps.invert(image)
 
-def adjust_tone_curve(image, curve):
-    img_array = np.array(image)
-    for channel in range(3):
-        img_array[:, :, channel] = np.interp(img_array[:, :, channel], np.arange(256), curve[channel])
-    return Image.fromarray(img_array)
+# トーンカーブ
+def apply_tone_curve(image, curve_points):
+    r_curve, g_curve, b_curve = curve_points
+    r, g, b = image.split()
+    r = Image.eval(r, lambda x: int(np.interp(x, range(256), r_curve)))
+    g = Image.eval(g, lambda x: int(np.interp(x, range(256), g_curve)))
+    b = Image.eval(b, lambda x: int(np.interp(x, range(256), b_curve)))
+    return Image.merge("RGB", (r, g, b))
 
-def plot_tone_curve(curve):
+def plot_tone_curve(curve_points):
+    r_curve, g_curve, b_curve = curve_points
     x = np.arange(256)
     plt.figure(figsize=(6, 4))
-    plt.plot(x, curve[0], 'r', label='R')
-    plt.plot(x, curve[1], 'g', label='G')
-    plt.plot(x, curve[2], 'b', label='B')
+    plt.plot(x, r_curve, 'r', label='R')
+    plt.plot(x, g_curve, 'g', label='G')
+    plt.plot(x, b_curve, 'b', label='B')
     plt.title('トーンカーブ')
     plt.xlabel('入力')
     plt.ylabel('出力')
     plt.legend()
-    st.pyplot()
+    return plt
 
 def main():
-    st.set_page_config(page_title="画像処理アプリ", page_icon="🎨")
+    st.set_page_config(page_title="画像処理アプリVer.kk", page_icon="🎨")
 
     st.title("画像処理アプリ")
 
     uploaded_image = st.file_uploader("画像をアップロードしてください", type=["jpg", "jpeg", "png"])
 
     if uploaded_image is not None:
-        st.sidebar.title("オプション")
-        operation = st.sidebar.selectbox("処理を選択", ["高画質化", "コントラスト調整", "色反転化", "トーンカーブ調整"])
+        st.subheader("元の画像")
+        st.image(uploaded_image, caption="元の画像", use_column_width=True)
 
-        original_image = Image.open(uploaded_image)
-        st.subheader("変更前")
-        st.image(original_image, caption="変更前", use_column_width=True)
+        image = Image.open(uploaded_image)
 
-        if operation == "高画質化":
-            enhanced_image = enhance_image(original_image)
-        elif operation == "コントラスト調整":
-            contrast_factor = st.sidebar.slider("コントラスト調整", 0.5, 2.0, 1.0, 0.1)
-            enhanced_image = adjust_contrast(original_image, contrast_factor)
-        elif operation == "色反転化":
-            enhanced_image = invert_colors(original_image)
+        st.sidebar.header("画像処理オプション")
+        options = {
+            "高画質化": enhance_image,
+            "コントラスト調整": adjust_contrast,
+            "色反転化": invert_colors,
+            "トーンカーブ": apply_tone_curve,
+        }
+        selected_option = st.sidebar.selectbox("処理を選択してください", list(options.keys()))
+
+        if selected_option == "トーンカーブ":
+            st.sidebar.subheader("トーンカーブ設定")
+            r_curve = st.sidebar.slider("Rトーンカーブ", 0, 255, (0, 255))
+            g_curve = st.sidebar.slider("Gトーンカーブ", 0, 255, (0, 255))
+            b_curve = st.sidebar.slider("Bトーンカーブ", 0, 255, (0, 255))
+            curve_points = [r_curve, g_curve, b_curve]
+            processed_image = options[selected_option](image, curve_points)
+            
+            st.subheader("トーンカーブの状態")
+            st.pyplot(plot_tone_curve(curve_points))
         else:
-            st.sidebar.subheader("トーンカーブ調整")
-            r_curve = st.sidebar.slider("R チャンネル", 0.0, 2.0, 1.0, 0.1)
-            g_curve = st.sidebar.slider("G チャンネル", 0.0, 2.0, 1.0, 0.1)
-            b_curve = st.sidebar.slider("B チャンネル", 0.0, 2.0, 1.0, 0.1)
-            curve = [r_curve, g_curve, b_curve]
-            enhanced_image = adjust_tone_curve(original_image, curve)
-            plot_tone_curve(curve)
+            processed_image = options[selected_option](image)
 
-        st.subheader("変更後")
-        st.image(enhanced_image, caption="変更後", use_column_width=True)
-
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-        ax1.imshow(original_image)
-        ax1.set_title("変更前")
-        ax1.axis("off")
-        ax2.imshow(enhanced_image)
-        ax2.set_title("変更後")
-        ax2.axis("off")
-
-        st.subheader("変更前後比較")
-        st.pyplot(fig)
+        st.subheader("処理後の画像")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(image, caption="変更前", use_column_width=True)
+        with col2:
+            st.image(processed_image, caption="変更後", use_column_width=True)
 
 if __name__ == "__main__":
     main()
