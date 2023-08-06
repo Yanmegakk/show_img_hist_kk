1bba3e1c507e73f3f9ec8e3eee08c312c36766be
kk <kaede112053@gmail.com>
2023-08-06 17:24:17 +0900

    palette4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageEnhance
 from colorthief import ColorThief
 import matplotlib.pyplot as plt
 import numpy as np
+import io
 
 def enhance_image(image, scale_factor=2):
     width, height = image.size
@@ -49,22 +50,17 @@ def main():
         for effect in selected_effects:
             if effect == "高画質化":
                 enhanced_image = enhance_image(enhanced_image)
-            elif effect == "コントラスト調整":
-                contrast_factor = st.sidebar.slider("コントラスト調整", 0.1, 3.0, 1.0)
-                enhanced_image = adjust_contrast(enhanced_image, contrast_factor)
-            elif effect == "色反転化":
-                enhanced_image = invert_colors(enhanced_image)
-            elif effect == "HSVパラメータの調節":
-                h_factor = st.sidebar.slider("Hue", 0.0, 2.0, 1.0)
-                s_factor = st.sidebar.slider("Saturation", 0.0, 2.0, 1.0)
-                v_factor = st.sidebar.slider("Value", 0.0, 2.0, 1.0)
-                enhanced_image = adjust_hsv(enhanced_image, h_factor, s_factor, v_factor)
+            # ... 以前のエフェクトはそのまま ...
 
         st.subheader("変更後の画像")
         st.image(enhanced_image, caption="変更後の画像", use_column_width=True)
 
         st.sidebar.header("カラーパレット")
-        color_thief = ColorThief(enhanced_image)
+        # BytesIOを使用して画像を一時的に保存
+        img_io = io.BytesIO()
+        enhanced_image.save(img_io, format='PNG')
+        img_io.seek(0)
+        color_thief = ColorThief(img_io)
         palette = color_thief.get_palette(color_count=5)
         plot_color_palette(palette)
 
